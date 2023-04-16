770e1056d9ae8ce1d495e2df68ead1b8826e7231
Teiyem <teethedev@gmail.com>
2023-04-16 21:44:19 +0200

    Added application user model

---
+++ Hover/Models/AppUser.cs
@@ -0,0 +1,22 @@
+﻿namespace Hover.Models;
+
+/// <summary>
+/// Application User model class.
+/// </summary>
+public class AppUser
+{
+    /// <summary>
+    /// Gets or sets the application user's id.
+    /// </summary>
+    public int? Id { get; set; }
+
+    /// <summary>
+    /// Gets or sets the application user's username.
+    /// </summary>
+    public string? Username { get; set; }
+
+    /// <summary>
+    /// Gets or sets the application user's password.
+    /// </summary>
+    public string? Password { get; set; }
+}
