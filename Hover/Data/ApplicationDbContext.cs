e54966419888971813623683afb9d6ecf02b24a0
Teiyem <teethedev@gmail.com>
2023-04-16 21:44:57 +0200

    Added application db context

---
+++ Hover/Data/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Hover.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Hover.Data;
+
+/// <summary>
+/// Represents the database context for the application, which provides access to the database through Entity Framework Core.
+/// </summary>
+public class ApplicationDbContext : DbContext
+{
+    /// <summary>
+    /// Initializes a new instance of the <see cref="ApplicationDbContext"/> class with the specified options.
+    /// </summary>
+    /// <param name="options">The options to be used by the context.</param>
+    public ApplicationDbContext(DbContextOptions options) : base(options) { }
+
+    /// <summary>
+    /// Gets or sets the database set for the application users.
+    /// </summary>
+    public DbSet<AppUser> AppUsers { get; set; }
+}
